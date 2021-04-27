693829bc360f02ba6cdb672229fe10e8c3bdcdb3
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-27 09:45:15 -0600

    Destructure props, add key to component

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Orders.css';
 
-const Orders = props => {
-  const orderEls = props.orders.map(order => {
+const Orders = ({ orders }) => {
+  const orderEls = orders.map(order => {
     return (
-      <div className="order">
+      <div key={order.id} className="order">
         <h3>{order.name}</h3>
         <ul className="ingredient-list">
           {order.ingredients.map(ingredient => {
