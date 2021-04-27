a7357dce07a21de86138e7a952787068777eb7ac
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-27 09:44:22 -0600

    Add props to state, create/pass down submit functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+import { getOrders, postOrders } from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
 class App extends Component {
   constructor(props) {
     super();
+    this.state = {
+      orders: []
+    };
+  }
+
+  submitOrder = (order) => {
+    postOrders(order)
+      .then(ordersData => this.setState({orders: ordersData.orders}))
+      .catch(error => console.error(error))
   }
 
   componentDidMount() {
     getOrders()
+      .then(ordersData => this.setState({ orders: ordersData.orders }))
       .catch(err => console.error('Error fetching:', err));
   }
 
@@ -19,7 +29,7 @@ class App extends Component {
       <main className="App">
         <header>
           <h1>Burrito Builder</h1>
-          <OrderForm />
+          <OrderForm submitOrder={this.submitOrder} />
         </header>
 
         <Orders orders={this.state.orders}/>
