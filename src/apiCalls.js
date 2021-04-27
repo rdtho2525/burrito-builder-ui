8ea4dc567f25d1cad21f90f43b2db0fef73c2ed4
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-27 09:43:50 -0600

    Add, export POST fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,14 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export const postOrders = (order) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(order)
+    })
+      .then(response => response.json())
+}
